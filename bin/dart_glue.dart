59432d07b3147fa7a89dc87ab4dd598b8e5dff52
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2024-06-15 08:37:24 +0200

    initial code

---
+++ bin/dart_glue.dart
@@ -0,0 +1,5 @@
+import 'package:dart_glue/dart_glue.dart' as dart_glue;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_glue.calculate()}!');
+}
---
+++ test/dart_glue_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_glue/dart_glue.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
