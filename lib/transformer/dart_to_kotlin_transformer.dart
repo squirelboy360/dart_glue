293d069158c6aae9266b461d7bf1569ed78dc2cc
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2024-08-14 11:41:16 +0200

    Update dart_to_kotlin_transformer.dart

--- lib/transformer/dart_to_kotlin_transformer.dart
+++ lib/transformer/dart_to_kotlin_transformer.dart
@@ -7,8 +7,8 @@ class DartToKotlinTransformer {
 
   String transform(String dartCode) {
     // Logic to transform Dart Flutter-style widgets to Jetpack Compose
-    // Example: Mapping Stateful and Stateless widgets to Jetpack Compose components
-    var transformedCode = ''; // Implement transformation logic here
+   
+    var transformedCode = ''; 
 
     return transformedCode;
   }
